8c7970a672f134de97849a5347b399536624ac17
hnaga84 <sfbnv17@gmail.com>
2022-09-17 18:51:43 +0900

    itemクラスをItemにリネーム

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesFragment.kt
@@ -28,7 +28,7 @@ class RepositoriesFragment: Fragment(R.layout.fragment_repositories){
         val dividerItemDecoration=
             DividerItemDecoration(context!!, layoutManager.orientation)
         val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
-            override fun itemClick(item: item){
+            override fun itemClick(item: Item){
                 gotoRepositoryFragment(item)
             }
         })
@@ -53,7 +53,7 @@ class RepositoriesFragment: Fragment(R.layout.fragment_repositories){
         }
     }
 
-    fun gotoRepositoryFragment(item: item)
+    fun gotoRepositoryFragment(item: Item)
     {
         val action= RepositoriesFragmentDirections
             .actionRepositoriesFragmentToRepositoryFragment(item= item)
@@ -61,13 +61,13 @@ class RepositoriesFragment: Fragment(R.layout.fragment_repositories){
     }
 }
 
-val diff_util= object: DiffUtil.ItemCallback<item>(){
-    override fun areItemsTheSame(oldItem: item, newItem: item): Boolean
+val diffUtil= object: DiffUtil.ItemCallback<Item>(){
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean
     {
         return oldItem.name== newItem.name
     }
 
-    override fun areContentsTheSame(oldItem: item, newItem: item): Boolean
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean
     {
         return oldItem== newItem
     }
@@ -76,12 +76,12 @@ val diff_util= object: DiffUtil.ItemCallback<item>(){
 
 class CustomAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<item, CustomAdapter.ViewHolder>(diff_util){
+) : ListAdapter<Item, CustomAdapter.ViewHolder>(diffUtil){
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view)
 
     interface OnItemClickListener{
-    	fun itemClick(item: item)
+    	fun itemClick(item: Item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
