53e08b861e4f1fc1f36becaf34ba1d871b403495
hnaga84 <sfbnv17@gmail.com>
2022-09-19 19:53:52 +0900

    不要なコメントを削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
@@ -43,9 +43,6 @@ class RepositoriesViewModel(
 
             val items = mutableListOf<Item>()
 
-            /**
-             * アイテムの個数分ループする
-             */
             for (i in 0 until jsonItems.length()) {
                 val jsonItem = jsonItems.optJSONObject(i)
                 val name = jsonItem.optString("full_name")
