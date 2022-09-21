10c58e7b1059bd0854d277b33a52530714037d33
hnaga84 <sfbnv17@gmail.com>
2022-09-21 21:26:21 +0900

    CoroutineExceptionHandlerで例外を処理するように変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoriesViewModel.kt
@@ -3,14 +3,15 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.model.Repository
 import jp.co.yumemi.android.code_check.net.GithubAppRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import retrofit2.HttpException
+import java.net.UnknownHostException
 import java.util.*
 
 /**
@@ -20,12 +21,43 @@ class RepositoriesViewModel : ViewModel(
 
 ) {
     var repositories: MutableLiveData<List<Repository>> = MutableLiveData<List<Repository>>()
+    val searchError: MutableLiveData<Throwable> = MutableLiveData<Throwable>()
+    var alertTitle: String? = null
+    var alertMessage: String? = null
+
     private val githubAppRepository: GithubAppRepository = GithubAppRepository()
 
-    fun searchRepositories(inputText: String) {
-        viewModelScope.launch(Dispatchers.IO) {
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
+    private val coroutineExceptionHandler =
+        CoroutineExceptionHandler { _, throwable ->
+            when (throwable) {
+                is HttpException -> {
+                    alertTitle = "通信エラー"
+                    alertMessage = "時間をおいてから再度お試しください"
+                    searchError.postValue(throwable)
+                }
+                is UnknownHostException -> {
+                    alertTitle = "通信エラー"
+                    alertMessage = "インターネットの接続を確認してください"
+                    searchError.postValue(throwable)
+                }
+                else -> {
+                    Log.d("error", throwable.printStackTrace().toString())
+                }
+            }
+        }
+
+    private fun fetchRepositories(inputText: String) {
+        scope.launch(coroutineExceptionHandler) {
             repositories.postValue(githubAppRepository.getRepositories(query = inputText)?.items)
             lastSearchDate = Date()
+        }
+    }
+
+    fun search(inputText: String) {
+        if (inputText.isNotEmpty()) {
+            fetchRepositories(inputText)
         }
     }
 
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/net/GithubAppRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/net/GithubAppRepository.kt
@@ -1,10 +1,8 @@
 package jp.co.yumemi.android.code_check.net
 
-import android.util.Log
 import jp.co.yumemi.android.code_check.model.MyResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
 
 class GithubAppRepository() {
     private var service: GithubApiInterface = Retrofit.Builder()
@@ -14,18 +12,7 @@ class GithubAppRepository() {
         .create(GithubApiInterface::class.java)
 
     fun getRepositories(query: String?): MyResponse? {
-        try {
-            val response = service.getRepositories(query).execute()
-
-            if (response.isSuccessful) {
-                return response.body()
-            } else {
-                Log.d("GithubAppRepository", "GET ERROR")
-            }
-
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return null
+        val response = service.getRepositories(query).execute()
+        return response.body()
     }
 }
